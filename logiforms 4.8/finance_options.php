dc1e0dbe2a153176725b4a605a922ae36fd8c42f
Julian Josephs <josephs.julian@gmail.com>
2010-10-21 00:33:48 -0500

    added 2 files and modified the new css file 2

---
+++ logiforms 4.8/finance_options.php
@@ -0,0 +1,49 @@
+<?php require_once('form_header.php'); ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<?php 
+	$titled="Hearing Aids Finance Options";
+	include("includes/header.php");
+?>
+</head>
+
+<body>
+<div id="container">
+<?php require_once('inc_header.php'); ?>
+
+<?php require_once('inc_top.php'); ?>
+
+
+<div id="main">
+
+<?php require_once('inc_maintopinner.php'); ?>
+
+
+<div id="column1">
+<?php require_once('inc_report.php'); ?>
+
+<?php require_once('inc_ringing.php'); ?>
+
+<div id="column4">
+<h1>Affordable Hearing Healthcare with<br />
+  Convenient Payment Plans</h1>
+
+<p><strong><a target='_blank' href="http://www.carecredit.com"><img src="images/carecredit.gif" border="0" class="fltright"/></a>Care Credit: Low Monthly Payment Plans</strong></p>
+<p>We offer CareCredit because we're committed to your hearing health.  CareCredit is a convenient, low minimum monthly payment program allowing you to purchase your hearing devices today.  With no up-front costs, no pre-payment penalties, and no annual fees, CareCredit lets you pay over time and frees up cash and credit cards for the other things you need.  It's easy with immediate approval decisions.</p>
+<p><strong>Better Hearing Plan</strong></p>
+<p>The Better Hearing Plan extends an exclusive hearing benefit offering free testing and discounted prices on hearing products to employees and their friends and family. It provides access to a relationship with the most experienced professionals in the industry, top quality patient care and exceptional technology. If you are an employer or employee of a local company that is interested in this free benefit package, please contact our practice today for more information.</p>
+<p><strong>Insurance</strong></p>
+<p>Certain insurance companies have a benefit towards the purchase of your hearing instrument. Better Sound Audiology & Hearing Aid Service will assist you in determining a benefit that can assist with the cost of the instruments and in many cases directly bill your insurance for payment. Better Sound Audiology & Hearing Aid Service participates with many providers who offer assistance toward  payment of your hearing instruments. </p>
+
+<br />
+</div>
+
+<?php require_once('inc_footer.php'); ?>
+
+
+</div><!--END MAIN-->
+</div><!--END CONTAINER-->
+<?php require_once('inc_endscripts.php'); ?>
+</body>
+</html>
